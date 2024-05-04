ac36dfd349944fd9a7f66d61e8c39ed80f4ec5d5
RickGrimesCodes <161536912+RickGrimesCodes@users.noreply.github.com>
2024-05-03 20:26:00 -0400

    FINAL SUBMISSION

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,10 @@
-"Super duper data analytics machine calculator thingy python part 1"
+#Super duper data analytics machine calculator thingy python part 1
 import os
 import csv
 
-"here goes somthing... maybe"
+#here goes somthing... maybe
 PyBankCSV = os.path.join('Resources' , 'budget_data.csv')
+# My goal was to use as few for loops as posable, so I converted column 2 into a list.
 with open(PyBankCSV, 'r') as PyBanks:
     csvreader = csv.reader(PyBanks)
     next(csvreader, None)
@@ -13,9 +14,11 @@ rowCount = len(column2List)
 ProfitTotal = sum(column2List)
 ProfitMax = max(column2List)
 ProfitMin = min(column2List)
-
-changes = [column2List[i+1] - column2List[i] for i in range(len(column2List)-1)]
+# Had to use another for loop, this takes the data of column 2 and figures out the change between each entry, thanks list comprehension!
+# https://www.codecademy.com/article/list-comprehension
+changes = [column2List[entry+1] - column2List[entry] for entry in range(len(column2List)-1)]
 averageChange = sum(changes) / len(changes)
+# final touches
 output =(
     f"\nFinacial Analysis \n"
     f"{'-'*25}"
@@ -25,7 +28,7 @@ output =(
     f"Greatest Increase in Profits: ${ProfitMax}\n"
     f"Greatest Decrease in profits: ${ProfitMin}\n"
     )
-
+# print and create a output txt file
 print(output)
 FileOutput = os.path.join('Analysis' , 'PyBankOutput.txt')
 with open (FileOutput,'w') as file:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,11 +7,11 @@ with open(pyPollCSV, 'r') as PyPoll:
     csvreader = csv.reader(PyPoll)
     next(csvreader, None)
     column3List = [row[2] for row in csvreader ]
-# making a dictionary to store the votes count with a for if loop
+
 rowCount =(len(column3List))
 
 votecount = {}
-
+# making a dictionary to store the votes count with a for if loop, Dr. A loves dictionaries.
 for items in column3List:
     if items in votecount:
         votecount[items] += 1
@@ -19,11 +19,11 @@ for items in column3List:
         votecount[items] = 1
 votecount = votecount
 winner = max(votecount, key=votecount.get)
-
+# another for loop that creates a entry with politician, vote percentage, and votecount, and dumps it into a 'temporary' output string.
 outputTemp = ""
 for items, votecount in votecount.items():
     outputTemp += (f"{items}: {((votecount/ rowCount)*100):,.2f}% ({votecount})\n")
-
+# final touches
 output = ("Election Results \n\n"
 f"{"-"*30}\n\n"
 f"Total Votes: {rowCount}\n\n"
@@ -32,7 +32,7 @@ f"{outputTemp}\n"
 f"{"-"*30}"
 f"\n\nDING DING DING DING!!! WINNER!!!: {winner}"
 )
-
+# create a file and print
 FileOutput = os.path.join('Analysis' , 'PyPollOutput.txt')
 with open (FileOutput,'w') as file:
     file.write(output)
